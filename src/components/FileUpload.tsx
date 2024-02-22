f5f7edaf76d913cdad455c3b00955ab7e63e3258
codewithwaleed <codewithwaleed@gmail.com>
2024-02-23 01:00:30 +0500

    file uploader configured

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -1,9 +1,31 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
+import { useDropzone } from "react-dropzone";
+import {Inbox} from "lucide-react"
 const FileUpload = () => {
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: {"application/pdf": ['.pdf']},
+    maxFiles: 1,
+    onDrop: (acceptedFiles) => {
+        console.log(acceptedFiles, 'acceptedFiles');
+    }
+  });
   return (
-    <div>FileUpload</div>
-  )
-}
+    <div className="p-2 bg-white rounded-xl">
+      <div
+        {...getRootProps({
+          className:
+            "border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col",
+        })}
+      >
+        <input {...getInputProps()} />
+        <>
+            <Inbox />
+            <p className="mt-2 text-sm text-slate-400">Drop your PDF here.</p>
+        </>
+      </div>
+    </div>
+  );
+};
 
-export default FileUpload
+export default FileUpload;
