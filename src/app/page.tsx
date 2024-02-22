245f436da98828e3c4b959c04e221bebe2e27459
codewithwaleed <codewithwaleed@gmail.com>
2024-02-22 23:33:27 +0500

    clerk, neondb and drizzzle setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,40 @@
 import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { UserButton, auth } from "@clerk/nextjs";
+import Link from "next/link";
+import {LogIn} from "lucide-react"
+export default async function Home() {
+  const { userId } = await auth();
+  const isAuth = !!userId;
+  return (
+    <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+        <div className="flex flex-col items-center text-center">
+          <div className="flex items-center">
+            <h1 className="text-5xl font-semibold">
+              Read any PDf and ask anything.{" "}
+            </h1>
+            <UserButton afterSignOutUrl="/" />
+          </div>
+          <div className="flex mt-2">
+            {
+              isAuth && (
+                <Button>Go to Chats</Button>
 
-export default function Home() {
-  return <Button>Click Me.</Button>
+              )
+            }
+          </div>
+          <p className="max-w-xl mt-1 text-lg text-slate-600" >Join millions of user to read through your pdf.</p>
+          <div className="w-full mt-4">
+            {
+              isAuth ? (
+                <h1>File upload</h1>
+              ) : (
+                <Link href="sign-in"><Button>Login to get started! <LogIn className="w-4 ml-2 h-4" /> </Button></Link>
+              )
+            }
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
